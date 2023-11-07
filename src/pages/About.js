2fbbc53ad680de47e033b276c11cc37cdb53c7c0
AAmitha <128313037+AAmitha@users.noreply.github.com>
2023-11-07 22:02:34 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./about.css";
 import Info from "../components/Info";
 import { FaDownload } from "react-icons/fa";
-import CV from "../assets/Amitha_AkepatiResume.pdf"
+import CV from "../assets/Amitha_Akepati_Resume.pdf"
 import Skills from "../components/Skills";
 import A from '../assets/About.gif';
 import { resume } from "../data";
